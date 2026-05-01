7b772384b6c91e0a3c9b2d6ebdd84ec75817ce08
extrahard1613 <extrahard1703@gmail.com>
2026-05-02 00:27:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,54 @@ import {
     detectBattleLoading,
     detectAccountInformationChanges
 } from "./multipleAccount/index.js";
-  
+
+let injectedElements = document.getElementById("modal-root");
+injectedElements.innerHTML = `
+  <div class='multipleAccount-popupContainer'>
+      <div class='accountMenu-container'></div>
+      <div class='multipleAccount'>
+          <div class='multipleAccount-header'>
+              <span>Аккаунты</span>
+              <div class='closeMultipleAccountButton-Container'>
+                  <div class='closeMultipleAccountButton'></div>
+              </div>
+          </div>
+          <div class='multipleAccount-navigation'>
+              <div class='accountListTab-conatiner'>
+                  <div class='accountListTab' id='firstTab'>
+                      <span id='firstTab-textContent'>Все</span>
+                      <div class='activeTabUnderline' id='firstActiveTabUnderline'></div>
+                  </div>
+                  <div class='accountListTab'  id='secondTab'>
+                      <span id='secondTab-textContent'>Группа А</span>
+                      <div class='activeTabUnderline' id='secondActiveTabUnderline'></div>
+                  </div>
+                  <div class='accountListTab' id='thirdTab'>
+                      <span id='thirdTab-textContent'>Группа Б</span>
+                      <div class='activeTabUnderline' id='thirdActiveTabUnderline'></div>
+                  </div>
+              </div>
+              <div class='searchAccountInput-container'>
+                  <input type='text' placeholder='найти' class='searchAccountInput'>
+                  <div class='searchAccountInput-backgroundImage'></div>
+              </div>    
+          </div>
+          <div class='multipleAccount-main'>
+              <table class='accountList-table'>
+                  <tbody class='accountList-tbody' id='firstAccountList'>
+                  </tbody>
+                  <tbody class='accountList-tbody' id='secondAccountList'>
+                  </tbody>
+                  <tbody class='accountList-tbody' id='thirdAccountList'>
+                  </tbody>
+              </table>
+          </div>
+          <div class='multipleAccount-footer'>
+              <div class='addAccountButton'>Добавить аккаунт</div>
+          </div>
+      </div>
+  </div>`
+
 function lookingForTancoins() {
     let lastState = false;
     let timeout = null;
