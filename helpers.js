7a0c18527693a983bc5c112cfb69a64dc4b55fe0
extrahard1613 <extrahard1703@gmail.com>
2026-05-03 17:03:34 +0200

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -8,7 +8,7 @@ export function simulateKeyPress(key) {
     document.activeElement?.dispatchEvent(keyPressEvent);
   }
   
-  export function simulateKeyPress2(key) {
+export function simulateKeyPress2(key) {
     const keyDownEvent = new KeyboardEvent('keydown', { 'key': key, code: `Key${key.toUpperCase()}`, keyCode: key.toUpperCase().charCodeAt(0), bubbles: true });
     const keyUpEvent = new KeyboardEvent('keyup', { 'key': key, code: `Key${key.toUpperCase()}`, keyCode: key.toUpperCase().charCodeAt(0), bubbles: true });
     const keyPressEvent = new KeyboardEvent('keypress', { 'key': key, code: `Key${key.toUpperCase()}`, keyCode: key.toUpperCase().charCodeAt(0), bubbles: true });
@@ -20,27 +20,28 @@ export function simulateKeyPress(key) {
     document.activeElement?.dispatchEvent(keyPressEvent);
   }
 
-  export function expectElement(container, selectors, returnIfPresent = false) {
-
-    if (returnIfPresent) {
-        const element = container.querySelector(selectors);
-        if (element) return Promise.resolve(element);
+export function expectElement(selector, parent = document) {
+    if (typeof selector !== 'string') {
+        console.error("Ошибка: селектор должен быть строкой, а пришло:", selector);
+        return Promise.reject("Invalid selector");
     }
-      return new Promise((resolve) => {
-          new MutationObserver((mutations, observer) => {
-              const addedNodes = mutations.flatMap((mutation) =>
-                  Array.from(mutation.addedNodes));
-              for (const addedNode of addedNodes) {
-                  if (!(addedNode instanceof Element)) continue;
-                  const element = addedNode.matches(selectors) ?
-                      addedNode : addedNode.querySelector(selectors);
-                  if (element) {
-                      resolve(element);
-                      observer.disconnect();
-                      return;
-                  }
-              }
-          }).observe(container,
-              { childList: true, subtree: true });
-      });
-  }
+
+    return new Promise((resolve) => {
+        const element = parent.querySelector(selector); 
+        
+        if (element) return resolve(element);
+
+        const observer = new MutationObserver((mutations, obs) => {
+            const el = parent.querySelector(selector);
+            if (el) {
+                obs.disconnect();
+                resolve(el);
+            }
+        });
+
+        observer.observe(parent instanceof HTMLDocument ? parent.documentElement : parent, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
