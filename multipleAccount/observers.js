c0fdfe2116eb1db6d548e58354ff7deb9c3a02aa
extrahard1613 <extrahard1703@gmail.com>
2026-05-03 17:01:22 +0200

    Update observers.js

--- multipleAccount/observers.js
+++ multipleAccount/observers.js
@@ -1,11 +1,12 @@
 import { closeAccountsList } from "./ui.js";
-import { replaceAccountInformation } from "./accountService.js";
+import { SELECTORS } from "./constants.js";
+import { updateTancoins } from "./accountService.js";
 
 export function detectBattleLoading(){
     let myMutationObserver = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
             mutation.addedNodes.forEach((node) => {
-                if (node instanceof Element && node.matches(".ApplicationLoaderComponentStyle-container")) {
+                if (node instanceof Element && node.matches(SELECTORS.LOBBY.LOADING)) {
                     closeAccountsList();
                     myMutationObserver.disconnect();
                 }
@@ -16,20 +17,38 @@ export function detectBattleLoading(){
     myMutationObserver.observe(document.documentElement, { childList: true, subtree: true });
 }
 
-export function detectAccountInformationChanges(){
-    let myMutationObserver = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            mutation.addedNodes.forEach((node) => {
-                if (node instanceof Element && node.matches(".FooterComponentStyle-accountsButton")) {
-                    setTimeout(() => {
-                        replaceAccountInformation();
-                        if (myMutationObserver)
-                            myMutationObserver.disconnect();
-                    }, 500);
-                }
-            });
-        });
+export function lookingForTancoins() {
+    let lastState = false;
+
+    const observer = new MutationObserver(() => {
+        const tankcoinContainer = document.querySelector(SELECTORS.SHOP.TANKCOINS_ICON);
+        const isShopOpen = !!tankcoinContainer;
+
+        if (isShopOpen && !lastState) {
+            updateTancoins();
+        }
+
+        lastState = isShopOpen;
     });
-    
-    myMutationObserver.observe(document.documentElement, { childList: true, subtree: true });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+}
+
+// Функция слежки за экраном входа
+export function watchForLogout() {
+    const observer = new MutationObserver(() => {
+        const loginForm = document.querySelector(SELECTORS.REGISTRATION.LOGIN);
+        
+        if (loginForm && window.location.hash) {
+            window.history.replaceState(null, "", window.location.origin + window.location.pathname);
+        }
+    });
+
+    observer.observe(document.documentElement, { 
+        childList: true, 
+        subtree: true 
+    });
 }
