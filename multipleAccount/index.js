e7a95b0b68a7838742bbb3229295f06449129b03
extrahard1613 <extrahard1703@gmail.com>
2026-05-03 17:00:32 +0200

    Update index.js

--- multipleAccount/index.js
+++ multipleAccount/index.js
@@ -1,3 +1,4 @@
-export { openAccountsList, closeAccountsList } from "./ui.js";
-export { detectBattleLoading, detectAccountInformationChanges } from "./observers.js";
-export { updateTancoins } from "./accountService.js";
+export { openAccountsList, closeAccountsList, initInterface } from "./ui.js";
+export { detectBattleLoading, lookingForTancoins, watchForLogout } from "./observers.js";
+export { replaceAccountInformation } from "./accountService.js";
+export { SELECTORS } from "./constants.js";
