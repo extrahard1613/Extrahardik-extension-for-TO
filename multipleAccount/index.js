0e6b8e6a2118478132e0870153db4216bf0c2643
extrahard1613 <extrahard1703@gmail.com>
2026-06-28 18:34:43 +0200

    Передана функция инициализации обсервера смены ника.

--- multipleAccount/index.js
+++ multipleAccount/index.js
@@ -1,4 +1,4 @@
 export { openAccountsList, closeAccountsList, initInterface } from "./ui.js";
 export { detectBattleLoading, lookingForTancoins, watchForLogout } from "./observers.js";
-export { replaceAccountInformation } from "./accountService.js";
+export { replaceAccountInformation, initNicknameObserver } from "./accountService.js";
 export { SELECTORS } from "./constants.js";
